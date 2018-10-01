361d2f3f94316ec8b28e82a515ee847377a3e339
chauhan130 <chauhan130@gmail.com>
2018-10-02 01:53:07 +0530

    Removed test measurement temporarily.

--- GitAPI/GitAPITracerTests/GitAPITracerTests.swift
+++ GitAPI/GitAPITracerTests/GitAPITracerTests.swift
@@ -43,12 +43,4 @@ class GitAPITracerTests: XCTestCase {
             }
         }
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
